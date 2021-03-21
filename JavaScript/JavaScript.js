d17dd7c3357416ce2571ad29bb2da57d2c21610c
motasimalazzam <motasim.h.alazzam@gmail.com>
2021-03-21 14:03:09 +0200

    add new features to the site

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -2,8 +2,22 @@ alert("Hello Travelers");
    var UserName = prompt('What is your name');
     alert("Welcome "+UserName+" to our Website")   ;
      document.write('<h3>'+"welcome "+UserName+'</h3>');
-   var place = prompt("Where would you like to spend Your vacation Petra, Wadi Rum or Aqaba");
-if(place=="Petra"){
+   var place = prompt("Where would you like to spend Your vacation in Petra, Wadi Rum or Aqaba?");
+
+while(place!='Petra'&& place!='Wadi rum'&& place!='Aqaba'){
+    place=prompt('pleas only write Petra or Wadi rum or Aqaba and dont forget using CAPITAL LETTER at begining');
+
+}
+
+var rating=prompt('rate the webSite out of 5');
+var star='<img src="http://clipart-library.com/images/kT8oyeMxc.jpg" width="250px">';
+var result='';
+for( var i=0; i<rating; i++){
+    result+=star;
+}
+document.write(result);
+
+   /*if(place=="Petra"){
     alert("Petra is a great choice for lovers of archeology and history");
 }
 else if(place == "Wadi rum"){
@@ -14,4 +28,4 @@ else if(place == "Wadi rum"){
     }
     else{
         alert("DONT FORGET USING A CAPITAL LETTER AT BEGINNING OF THE PLACE NAME" );
-    }
+    }*/
