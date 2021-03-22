bd32fd79145eb5392cca03c0f35d0b13ede9928f
motasimalazzam <motasim.h.alazzam@gmail.com>
2021-03-22 19:13:34 +0200

    adding CSS file

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,21 +1,38 @@
 alert("Hello Travelers");
+
    var UserName = prompt('What is your name');
-    alert("Welcome "+UserName+" to our Website")   ;
-     document.write('<h3>'+"welcome "+UserName+'</h3>');
-   var place = prompt("Where would you like to spend Your vacation in Petra, Wadi Rum or Aqaba?");
+   
+   function GetTheName(Name) {
+    alert("Welcome "+Name+" to our Website")   ;
+    // document.write('<h3>'+"Thank you "+UserName+'</h3>');   
+  }
+   GetTheName(UserName);
+    
 
-while(place!='Petra'&& place!='Wadi rum'&& place!='Aqaba'){
-    place=prompt('pleas only write Petra or Wadi rum or Aqaba and dont forget using CAPITAL LETTER at begining');
+     
+   var place = prompt("Where would you like to spend Your vacation in Petra, Wadi Rum or Aqaba?"); 
+     
+   function GetFavourtPlace(Fplace) {
+         
+     while(Fplace!='Petra'&& Fplace!='Wadi rum'&& Fplace!='Aqaba'){
+    Fplace=prompt('pleas only write Petra or Wadi rum or Aqaba and dont forget using CAPITAL LETTER at begining');
 
-}
+         }
+   }
+      GetFavourtPlace(place);
+     
+      var GetRate = function(){
+     var rating=prompt('rate the webSite out of 5');
+     var star='<img src="star.jpg" alt="img" width="150px">';
+     var result='';
+     document.write('<h3>'+"Thank you "+UserName+'</h3>'); 
+     for( var i=0; i<rating; i++){
+         result+=star;
+             }
+     return result
+    }
+     
 
-var rating=prompt('rate the webSite out of 5');
-var star='<img src="star.jpg" alt="img" width="250px">';
-var result='';
-for( var i=0; i<rating; i++){
-    result+=star;
-}
-document.write(result);
 
    /*if(place=="Petra"){
     alert("Petra is a great choice for lovers of archeology and history");
